bcdb062f09933d57de4d0febd277d6e55310f5e2
Theresa <thetheresagarcia@gmail.com>
2019-12-06 03:49:15 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,21 @@
-# Add your variables here
+
+
+def first_number(firstnum)
+  firstnum = gets.to_i
+end
+
+def second_number(secnum)
+  if secnum == 0
+    puts "enter a nonzero number"
+  end
+  secnum = gets.to_i
+end
+
+sum = #{firstnum} + #{secnum}
+
+difference = #{firstnum} - #{secnum}
+
+product = #{firstnum} * #{secnum}
+
+quotient = #{firstnum} / #{secnum}
+# do int.0 to make it a float?
