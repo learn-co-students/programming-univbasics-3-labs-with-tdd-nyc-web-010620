834fb5b13b0619b4d9bb4252ce63fda252a1e3ad
Mokhtar Ali <joemokhtarali@gmail.com>
2019-12-18 01:13:03 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,9 @@
-# Add your variables here
+def calculator.rb do
+  first_number = get_variable_from_file('./calculator.rb', "first_number")
+  second_number = get_variable_from_file('./calculator.rb', "second_number") && != 0
+  sum = first_number + second_number
+  difference = first_number - second_number
+  product = first_number * second_number
+  quotient = first_number / second_number
+  
+end
